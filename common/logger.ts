d0c25a55c08607176fe80a168c4f757f0e3a69d5
Wepeel <wepeelsuicune@gmail.com>
2021-04-08 12:20:52 +0300

    fixed logging formatting

--- common/logger.ts
+++ common/logger.ts
@@ -31,7 +31,7 @@ export const expressWinstonLogger = expressWinston.logger({
     ],
     format: winston.format.combine(
         winston.format.colorize(),
-        winston.format.json()
+        winston.format.simple()
     ),
     meta: false, // optional: control whether you want to log the meta data about the request (default to true)
     msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
