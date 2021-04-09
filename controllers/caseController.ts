dea538bd081da703395c1427fc6cba7e42f2f536
Wepeel <wepeelsuicune@gmail.com>
2021-04-09 16:59:11 +0300

    added logging for grpc

--- controllers/caseController.ts
+++ controllers/caseController.ts
@@ -4,8 +4,10 @@ import { logger } from '@global/logger';
 import { doctorClient } from "@global/grpc";
 
 
-export const casePost = (req: express.Request, res: express.Response) => {
+export const casePost = async (req: express.Request, res: express.Response) => {
     doctorClient.FindDisease(req.params, (err, response) => {
+        logger.error(err);
         logger.debug(response);
     });
+    res.send("BYE");
 };
