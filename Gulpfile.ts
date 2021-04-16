ed67e7fc856d3069514ba758edb01cb374bddf7d
Wepeel <wepeelsuicune@gmail.com>
2021-04-16 10:18:42 +0300

    finished fixing docker

--- Gulpfile.ts
+++ Gulpfile.ts
@@ -4,6 +4,32 @@ import { resolve, join } from 'path';
 import cp from 'child_process';
 import os from 'os';
 
+gulp.task("install", (cb: any) => {
+    // get library path
+    const lib = resolve(__dirname);
+
+    fs.readdirSync(lib).forEach((mod) => {
+        const modPath = join(lib, mod);
+
+        // ensure path has package.json
+        if (!fs.existsSync(join(modPath, 'package.json'))) {
+            return;
+        }
+
+        // npm binary based on OS
+        const npmCmd = os.platform().startsWith('win') ? 'npm.cmd' : 'npm';
+
+        // install folder
+        cp.spawn(npmCmd, ['i'], {
+            env: process.env,
+            cwd: modPath,
+            stdio: 'inherit'
+        });
+    });
+
+    cb();
+});
+
 
 gulp.task("build", (cb: any) => {
     // get library path
