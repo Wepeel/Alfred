e51b659ea96af52f80c2103a717d0ab7cee2b4dd
Wepeel <wepeelsuicune@gmail.com>
2021-04-16 09:53:49 +0300

    corrected docker compose volumes issue

--- UIBackend/Gulpfile.ts
+++ UIBackend/Gulpfile.ts
@@ -11,7 +11,7 @@ gulp.task("default", (cb: any) => {
 });
 
 gulp.task('build:tslint', () => {
-    return gulp.src(['UI Backend/**/*.ts'])
+    return gulp.src(['UIBackend/**/*.ts'])
         // eslint() attaches the lint output to the "eslint" property
         // of the file object so it can be used by other modules.
         .pipe(eslint())
